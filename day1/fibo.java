c439e44690165ce9e31fd36c8120fae4dff15511
Jatingupta9120 <jatinofficial9120@gmail.com>
2025-02-27 23:06:11 +0530

    Feat: Day1 Ready for Leetcode

---
+++ day1/fibo.java
@@ -0,0 +1,11 @@
+/*
+ Length of fibonacci series means previous two sum
+ x1,x2,x3,x4,x5 means x5 is the sum of x3+x4
+ x4=>x3+x2 like this 
+ */
+public Class fibo{
+    public static void main(String args[]){
+
+    }
+
+}
